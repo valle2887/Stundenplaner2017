0ab4e99b8b7cf4a1e1b64b7d020e42eb5b1f639b
admin <admin@sky>
2017-02-25 16:33:18 +0100

    Klasse NeuePruefung brauchen wir nicht mehr da in Klasse NeuenTerminHinzu Aufgabe, Veranstaltung und Prüfung alles in ein gui dargestellt Beim Auswählen zeigt er die notwendige lebel und Felder an.

--- Stundenplaner2017/source/gui/NeuenTerminHinzu.java
+++ Stundenplaner2017/source/gui/NeuenTerminHinzu.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionListener;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,6 +13,7 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+
 /**
  * @author Rakan Al-Swayyed
  */
@@ -22,238 +22,401 @@ public class NeuenTerminHinzu extends JFrame implements ActionListener {
      * generated serial Version ID.
      */
     private static final long serialVersionUID = 5043569320233706529L;
-    /**
-     * JButtongroupe terminTyp. 
-     */
-    private ButtonGroup terminTyp = new ButtonGroup();
-    /**
-     * JRadioButton radioVeranstaltung. 
-     */
-    private JRadioButton radioVeranstaltung = new JRadioButton("Veranstaltung");
-    /**
-     * JRadioButton radioAufgabe. 
-     */
-    private JRadioButton radioAufgabe = new JRadioButton("Aufgabe");
-    /**
-     * JRadioButton radioPruefung. 
-     */
-    private JRadioButton radioPruefung = new JRadioButton("Pruefung");
-    /**
-     * Label bezeichnung. 
-     */
-    private JLabel bezeichnung = new JLabel("Bezeichnung:");
-    /** 
-     * JTextField textBezeichnung. 
-     */
-    private JTextField textBezeichnung = new JTextField(20);
-    /**
-     * Label kategorie. 
-     */
-    private JLabel kategorie = new JLabel("Kategorie:");
-    /**
-     * Array vArray mit zwei unterschiedlichen kategorien Universität und 
-     * private. 
-     */
-    private String[] kaArray = {"Universität", "private"};
-    /**
-     * JComboBox kaKiste. 
-     */
-    private JComboBox<Object> kaKiste = new JComboBox<Object>(kaArray);
-    /**
-     * Label datum. 
-     */
-    private JLabel datum = new JLabel("Datum:");
-    /**
-     * Array tagArray mit 31 tage. 
-     */
-    private String[] tagArray = {"1", "2", "3", "4", "5", "6", "7", "8", "9", 
-        "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", 
-        "22", "23", "24", "25", "26", "27" , "28", "29", "30", "31"};
-    /**
-     * JComboBox tagKiste. 
-     */
-    private JComboBox<Object> tagKiste = new JComboBox<Object>(tagArray);
-    /**
-     * Array monatArray mit 12 Monate. 
-     */
-    private String[] monatArray = {"1", "2", "3", "4", "5", "6", "7", "8", "9",
-        "10", "11", "12"};
-    /**
-     * JComboBox monatKiste. 
-     */
-    private JComboBox<Object> monatKiste = new JComboBox<Object>(monatArray);
-    /**
-     * Array jahrArray mit 6 Jahre. 
-     */
-    private String[] jahrArray = {"2017", "2018", "2019", "2020", "2021",
-        "2022"};
-    /**
-     * JComboBox jahrKiste. 
-     */
-    private JComboBox<Object> jahrKiste = new JComboBox<Object>(jahrArray);
-    /**
-     * Label uhrzeit. 
-     */
-    private JLabel uhrzeit = new JLabel("Uhrzeit:");
-    /**
-     * Array stdArray mit 24 Stunden. 
-     */
-    private String[] stdArray = {"1", "2", "3", "4", "5", "6", "7", "8", 
-        "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", 
-        "21", "22", "23", "00"};
-    
-    /**
-     * JComboBox stundenKiste. 
-     */
-    private JComboBox<Object> stundenKiste = new JComboBox<Object>(stdArray);
-    /**
-     * Array minArray mit 15 minuten Intervall. 
-     */
-    private String[] minArray = {"0", "15", "30", "45"};
-    /**
-     * JComboBox minutenKiste. 
-     */
-    private JComboBox<Object> minutenKiste = new JComboBox<Object>(minArray);
-    /**
-     * Label minP. 
-     */
-    private JLabel minP = new JLabel("min");
-    /**
-     * Label dauerP. 
-     */
-    private JLabel dauerP = new JLabel("Dauer:");
-    /**
-     * JTextField textDauer. 
-     */
-    private JTextField textDauerP = new JTextField(3);
-    /**
-     * Label dauerP. 
-     */
-    private JLabel notiz = new JLabel("Notiz:");
-    /**
-     * JTextArea notizArea. 
-     */
-    private JTextArea notizArea = new JTextArea(5, 20);
-    /**
-     * Label wiederholbar. 
-     */
-    private JLabel wiederholbar = new JLabel("wiederholen:");
-    /**
-     * JButtongroupe wiederJoN. 
-     */
-    private ButtonGroup wiederJoN = new ButtonGroup();
-    /**
-     * JRadioButton radioJa. 
-     */
-    private JRadioButton radioJa = new JRadioButton("nein");
-    /**
-     * JRadioButton radioNein. 
-     */
-    private JRadioButton radioNein = new JRadioButton("ja");
-    /**
-     * Array wieOftArray. 
-     */
-    private String[] wieOftArray = {"Einmalig", "Taglich", "Wöchenlich", 
-        "Monatlich"};
-    /**
-     * JComboBox wieOft ob einmalig, wochenlich, monatlich. 
-     */
-    private JComboBox<Object> wieOft = new JComboBox<Object>(wieOftArray);
-    //++++++++++++++++++++++++
-    /**
-     * JCheckBox tage. 
-     */
-    private JCheckBox mo, di, mi, don, fr, sa, so
 
-    
-    
-    //++++++++++++++++++++++++
+    /**
+     * JButtongroupe bgTerminTyp.
+     */
+    private static ButtonGroup bgTerminTyp = new ButtonGroup();
+
+    /**
+     * JRadioButton rbVeran.
+     */
+    private static JRadioButton rbVeran = new JRadioButton("Veranstaltung");
+
+    /**
+     * JRadioButton rbAufg.
+     */
+    private static JRadioButton rbAufg = new JRadioButton("Aufgabe");
+
+    /**
+     * JRadioButton rbPruef.
+     */
+    private static JRadioButton rbPruef = new JRadioButton("Pruefung");
+
+    /**
+     * Label lBezeichnung.
+     */
+    private JLabel lBezeichnung = new JLabel("Bezeichnung:");
+
+    /**
+     * JTextField tBezeichnung.
+     */
+    private JTextField tBezeichnung = new JTextField(20);
+
+    /**
+     * Label lKategorie.
+     */
+    private JLabel lKategorie = new JLabel("Kategorie:");
+
+    /**
+     * Array arrayKategorie mit zwei unterschiedlichen kategorien Universität 
+     * und private.
+     */
+    private String[] arrayKategorie = {"Universität", "private"};
+
+    /**
+     * JComboBox cbKategorie.
+     */
+    private JComboBox<Object> cbKategorie =
+        new JComboBox<Object>(arrayKategorie);
+
+    /**
+     * Label lDatum.
+     */
+    private JLabel lDatum = new JLabel("Datum:");
+
+    /**
+     * Array arrayTag mit 31 tage.
+     */
+    private String[] arrayTag = {"1", "2", "3", "4", "5", "6", "7", "8", "9",
+        "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21",
+        "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"};
+
+    /**
+     * JComboBox cbTag.
+     */
+    private JComboBox<Object> cbTag = new JComboBox<Object>(arrayTag);
+
+    /**
+     * Array arrayMonat mit 12 Monate.
+     */
+    private String[] arrayMonat =
+        {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
+
+    /**
+     * JComboBox cbMonat.
+     */
+    private JComboBox<Object> cbMonat = new JComboBox<Object>(arrayMonat);
+
+    /**
+     * Array arrayJahr mit 6 Jahre.
+     */
+    private String[] arrayJahr =
+        {"2017", "2018", "2019", "2020", "2021", "2022"};
+
+    /**
+     * JComboBox cbJahr.
+     */
+    private JComboBox<Object> cbJahr = new JComboBox<Object>(arrayJahr);
+
+    /**
+     * Label lUhrzeit.
+     */
+    private JLabel lUhrzeit = new JLabel("Uhrzeit:");
+
+    /**
+     * Array arrayStd mit 24 Stunden.
+     */
+    private String[] arrayStd =
+        {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
+            "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"};
+
+    /**
+     * JComboBox cbStunden.
+     */
+    private JComboBox<Object> cbStunden = new JComboBox<Object>(arrayStd);
+
+    /**
+     * Array arrayMinuten mit 15 minuten Intervall.
+     */
+    private String[] arrayMinuten = {"0", "15", "30", "45"};
+
+    /**
+     * JComboBox cbMinuten.
+     */
+    private JComboBox<Object> cbMinuten = new JComboBox<Object>(arrayMinuten);
+
+    /**
+     * Label lNotiz.
+     */
+    private JLabel lNotiz = new JLabel("Notiz:");
+
+    /**
+     * JTextArea tNotiz.
+     */
+    private JTextArea tNotiz = new JTextArea(5, 20);
+
+    /**
+     * Label lWiederh.
+     */
+    private JLabel lWiederh = new JLabel("wiederholen:");
+
+    /**
+     * JButtongroupe bgWieder.
+     */
+    private ButtonGroup bgWieder = new ButtonGroup();
+
+    /**
+     * JRadioButton rbJa.
+     */
+    private JRadioButton rbJa = new JRadioButton("nein");
+
+    /**
+     * JRadioButton rbNein.
+     */
+    private JRadioButton rbNein = new JRadioButton("ja");
+
+    /**
+     * Array arrayWieOft.
+     */
+    private String[] arrayWieOft =
+        {"Einmalig", "Taglich", "Wöchenlich", "Monatlich"};
+
+    /**
+     * JComboBox cbWieOft ob einmalig, wochenlich, monatlich.
+     */
+    private JComboBox<Object> cbWieOft = new JComboBox<Object>(arrayWieOft);
+
+    /**
+     * Label lMarker.
+     */
+    private JLabel lMarker = new JLabel("Marker:");
+
+    /**
+     * Array arrayMarker.
+     */
+    private String[] arrayMarker =
+        {"noch nicht", "noch nicht", "noch nicht", "noch nicht"};
+
+    /**
+     * JComboBox cbMarker fuer marken.
+     */
+    private JComboBox<Object> cbMarker = new JComboBox<Object>(arrayMarker);
+
+    /**
+     * Array arrayDauer.
+     */
+    private static String[] arrayDauer = {"30", "60", "90", "120", "240"};
+
+    /**
+     * JComboBox cbDauer.
+     */
+    private static JComboBox<Object> cbDauer = new JComboBox
+        <Object>(arrayDauer);
+
+    /**
+     * Label lMin.
+     */
+    private JLabel lMin = new JLabel("min");
+
+    /**
+     * Label lDauer.
+     */
+    private JLabel lDauer = new JLabel("Dauer:");
+
+    /**
+     * Label lEcts.
+     */
+    private JLabel lEcts = new JLabel("      ECTS");
+
+    /**
+     * JTextField tEcts.
+     */
+    private static JTextField tEcts = new JTextField(3);
+
+    /**
+     * Label lDozent.
+     */
+    private JLabel lDozent = new JLabel("      Dozent:");
+
+    /**
+     * JTextField tDozent.
+     */
+    private static JTextField tDozent = new JTextField(20);
+
+    /**
+     * Label lCampus.
+     */
+    private JLabel lCampus = new JLabel("      Campus:");
+
+    /**
+     * JTextField tCampus.
+     */
+    private static JTextField tCampus = new JTextField(20);
+
+    /**
+     * Label lRaum.
+     */
+    private JLabel lRaum = new JLabel("      Raum:    ");
+
+    /**
+     * JTextField tRaum.
+     */
+    private static JTextField tRaum = new JTextField(20);
     /**
      * JButton speichern.
      */
     private JButton speichern = new JButton("Speichern");
+
     /**
-     * JButtongroupe panelTerminTyp. 
+     * JButtongroupe p1.
      */
-    private JPanel panelTerminTyp = new JPanel();
+    private JPanel p1 = new JPanel();
+
     /**
-     * JPanel panelBuK. 
+     * JPanel p2.
      */
-    private JPanel panelBuK = new JPanel();
+    private JPanel p2 = new JPanel();
+
     /**
-     * JPanel panelDuU fuer Datum und Uhrzeit.
+     * JPanel p3.
      */
-    private JPanel panelDuU = new JPanel();
+    private JPanel p3 = new JPanel();
+
     /**
-     * JPanel panelNotiz fuer Notize.
+     * JPanel p4 .
      */
-    private JPanel panelNotiz = new JPanel();
-    /**
-     * JPanel panelWieder fuer Wiederholen.
-     */
-    private JPanel panelWieder = new JPanel();
+    private JPanel p4 = new JPanel();
     /**
      * Konstruktor der Klasse NeuenTerminHinzu .
      */
     public NeuenTerminHinzu() {
-        // TODO Auto-generated constructor stub
         setTitle("Termin Hinzufuegen");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new GridLayout(8, 1));
-        setResizable(false);
+        setLayout(new GridLayout(7, 1));
+        setResizable(true);
         setLocationRelativeTo(null);
-        // RadioButtons werden zu panelTerminTyp zugewiesen.
-        panelTerminTyp.add(radioVeranstaltung);
-        panelTerminTyp.add(radioAufgabe);
-        panelTerminTyp.add(radioPruefung);
-        terminTyp.add(radioVeranstaltung);
-        terminTyp.add(radioAufgabe);
-        terminTyp.add(radioPruefung);
-        //fuege zu panelBuK für bezeichnung und Kategorie.
-        panelBuK.add(bezeichnung);
-        panelBuK.add(textBezeichnung);
-        panelBuK.add(kategorie);
-        panelBuK.add(kaKiste);
-        //fuege zu panelDuU für datum und zeit. 
-        panelDuU.add(datum);
-        panelDuU.add(tagKiste);
-        panelDuU.add(monatKiste);
-        panelDuU.add(jahrKiste);
-        panelDuU.add(uhrzeit);
-        panelDuU.add(stundenKiste);
-        panelDuU.add(minutenKiste);
-        panelDuU.add(dauerP);
-        panelDuU.add(textDauerP);
-        panelDuU.add(minP);
-        //fuege zu panelNotiz fuer Notiz. 
-        panelNotiz.add(notiz);
-        panelNotiz.add(notizArea);
-        //fuege zu pannelWieder fuer Wiederholen.
-        panelWieder.add(wiederholbar);
-        panelWieder.add(radioNein);
-        panelWieder.add(radioJa);
-        panelWieder.add(wieOft);
-        wiederJoN.add(radioNein);
-        wiederJoN.add(radioJa);
+        // RadioButtons werden zu p1 zugewiesen.
+        p1.add(rbAufg);
+        p1.add(rbVeran);
+        p1.add(rbPruef);
+        bgTerminTyp.add(rbVeran);
+        bgTerminTyp.add(rbAufg);
+        bgTerminTyp.add(rbPruef);
         
-     // Hier werden die Container im Fenster angeordnet durch den Layoutmanager
-        add(panelTerminTyp);
-        add(panelBuK);
-        add(panelDuU);
-        add(panelNotiz);
-        add(panelWieder);
-        add(tage);
+        p2.add(lBezeichnung);
+        p2.add(tBezeichnung);
+        p2.add(lEcts);
+        p2.add(tEcts);
+        p2.add(lKategorie);
+        p2.add(cbKategorie);
+        p2.add(lDatum);
+        p2.add(cbTag);
+        p2.add(cbMonat);
+        p2.add(cbJahr);
+        p2.add(lUhrzeit);
+        p2.add(cbStunden);
+        p2.add(cbMinuten);
+        p2.add(lDauer);
+        p2.add(cbDauer);
+        p2.add(lMin);
+        p3.add(lWiederh);
+        p3.add(rbNein);
+        p3.add(rbJa);
+        p3.add(cbWieOft);
+        p3.add(lMarker);
+        p3.add(cbMarker);
+        p3.add(lNotiz);
+        p3.add(tNotiz);
+        bgWieder.add(rbNein);
+        bgWieder.add(rbJa);
+        p4.add(lDozent);
+        p4.add(tDozent);
+        p4.add(lCampus);
+        p4.add(tCampus);
+        p4.add(lRaum);
+        p4.add(tRaum);
+        p4.add(lMarker);
+        p4.add(cbMarker);
+        add(p1);
+        add(p2);
+        add(p3);
+        add(p4);
         add(speichern);
-        
+        machSichtbar();
         pack();
         setVisible(true);
+    }
+    /**
+     * methode machSichtbar() fuer Aufgabe, Veranstaltung und Pruefung.
+     */
+    public void machSichtbar() {
+        /**
+         * wenn rbAufg dann machSichtbar nix besonders. 
+         */
+        rbAufg.addActionListener(new ActionListener() {
+            /**
+             * @param e .
+             */
+            public void actionPerformed(ActionEvent e) {
+                lDauer.setVisible(false);
+                cbDauer.setVisible(false);
+                lMin.setVisible(false);
+                lCampus.setVisible(false);
+                tCampus.setVisible(false);
+                lRaum.setVisible(false);
+                tRaum.setVisible(false);
+                lDozent.setVisible(false);
+                tDozent.setVisible(false);
+                lEcts.setVisible(false);
+                tEcts.setVisible(false);
+                repaint();
+            }
+        });
+        /**
+         * wenn rbVeran dann machSichtbar Campus, Raum, Dozent und Ects.
+         */
+        rbVeran.addActionListener(new ActionListener() {
+            /**
+             * @param e .
+             */
+            public void actionPerformed(ActionEvent e) {
+                lDauer.setVisible(false);
+                cbDauer.setVisible(false);
+                lCampus.setVisible(true);
+                tCampus.setVisible(true);
+                lRaum.setVisible(true);
+                tRaum.setVisible(true);
+                lDozent.setVisible(true);
+                tDozent.setVisible(true);
+                lEcts.setVisible(true);
+                tEcts.setVisible(true);
+                repaint();
+            }
+        });
+        /**
+         * wenn rbVeran dann machSichtbar Dauer ,Campus und Raum.
+         */
+        rbPruef.addActionListener(new ActionListener() {
+            /**
+             * @param e.
+             */
+            public void actionPerformed(ActionEvent e) {
+                lDauer.setVisible(true);
+                cbDauer.setVisible(true);
+                lCampus.setVisible(true);
+                tCampus.setVisible(true);
+                lRaum.setVisible(true);
+                tRaum.setVisible(true);
+                lDozent.setVisible(false);
+                tDozent.setVisible(false);
+                lEcts.setVisible(false);
+                tEcts.setVisible(false);
+                repaint();
+            }
+        });
     }
     /**
      * @param e .
      */
     public void actionPerformed(ActionEvent e) {
         
-        
     }
+    /**
+     * @param args .
+     */
+    public static void main(String[] args) {
+        new NeuenTerminHinzu();
+    }
+}    
 
-}
