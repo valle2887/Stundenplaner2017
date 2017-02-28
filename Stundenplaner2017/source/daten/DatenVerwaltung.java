869c1894825c251c40532b6b59516304dd59cb54
valle <valle@DESKTOP-2LEDTLO.fritz.box>
2017-02-28 20:04:40 +0100

    korrektur

--- Stundenplaner2017/source/daten/DatenVerwaltung.java
+++ Stundenplaner2017/source/daten/DatenVerwaltung.java
@@ -15,36 +15,100 @@ public class DatenVerwaltung {
     /**
      * Passwort vergleichen.
      */
-    
-    public boolean vergleichPasswort(String name, String passwort,
-        ) throws FileNotFoundException
+
+    public boolean vergleichPasswort(String name, String passwort)
+        throws FileNotFoundException {
+
+        return false;
+
+    }
 
     /**
-     * Speichern von Benutzerdaten
+     * Speichern von Benutzerdaten.
      */
     public void speichernVonBenutzerdaten(String name, String passwort,
         String studiengang, int punkte) throws IOException {
+
         FileWriter schreiben = new FileWriter(name + ".txt", true);
 
-        schreiben.write(name);
+        schreiben.write("Username: \t" + name);
         schreiben.append(System.getProperty("line.separator"));
-        schreiben.write(passwort);
+        schreiben.write("Passwort: \t" + passwort);
         schreiben.append(System.getProperty("line.separator"));
-        schreiben.write(studiengang);
+        schreiben.write("Studiengang: \t" + studiengang);
         schreiben.append(System.getProperty("line.separator"));
-        schreiben.write(punkte);
+        schreiben.write("ECTS: \t" + punkte);
+        schreiben.append(System.getProperty("line.separator"));
         schreiben.append(System.getProperty("line.separator"));
         schreiben.close();
     }
 
     /**
-     * Speichern von Terminen in Datei
+     * Speichern von Terminen in Datei des Eingelogten Benutzers.
      */
+    public void speichernAufagbeArray(String[] termineArray, String name)
+        throws IOException {
+
+        FileWriter schreiben = new FileWriter(name + ".txt", true);
+
+        schreiben.write("Aufgabe");
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.write("Bezeichnung: \t" + termineArray[0]);
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.write("Datum: \t" + termineArray[1]);
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.write("Uhrzeit: \t" + termineArray[2]);
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.write("Dauer: \t" + termineArray[3]);
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.write("Notiz: \t" + termineArray[4]);
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.write(termineArray[5]);
+        // Bei Wiederholungen
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.write("Typ: \t" + termineArray[6]);
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.close();
+
+    }
 
     /**
-     * Speichern von Veranstaltung in Datei
+     * Speichern von Veranstaltung in Datei.
      */
+    public void speichernVeranstungsArray(String[] termineArray,
+        String[] veranstaltungsArray, String name) throws IOException {
 
+        FileWriter schreiben = new FileWriter(name + ".txt", true);
+
+        schreiben.write("Veranstaltung");
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.write("Bezeichnung \t" + termineArray[0]);
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.write("Datum: \t" + termineArray[1]);
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.write("Uhrzeit: \t" + termineArray[2]);
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.write("Dauer: \t" + termineArray[3]);
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.write("Notiz: \t" + termineArray[4]);
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.write(termineArray[5]);
+        // Bei Wiederholungen
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.write("Typ: \t" + termineArray[6]);
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.write("Raumnummer: \t" + veranstaltungsArray[0]);
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.write("Gebaude: \t" + veranstaltungsArray[1]);
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.write("Dozent: \t" + veranstaltungsArray[2]);
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.write("ECTS: \t" + veranstaltungsArray[3]);
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.append(System.getProperty("line.separator"));
+        schreiben.close();
+    }
     /**
      * Speichern von Prüfung in Datei
      */
