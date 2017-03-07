4f39dc8ea4de660003ed73ff0993f040495984e6
valle <valle@DESKTOP-2LEDTLO.fritz.box>
2017-03-07 16:19:00 +0100

    kalenderGui

--- Stundenplaner2017/source/gui/KalenderGui.java
+++ Stundenplaner2017/source/gui/KalenderGui.java
@@ -1,6 +1,8 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
@@ -17,7 +19,12 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+
+import daten.Aufgabe;
+import daten.Pruefung;
+import daten.Veranstaltung;
 
 /**
  * Klasse Gui.
@@ -31,56 +38,120 @@ public class KalenderGui extends JFrame implements ActionListener {
      */
     private JMenuBar menueBar = new JMenuBar();
 
+    /**
+     * 
+     */
     private JMenu info = new JMenu("Info");
 
+    /**
+     * 
+     */
     private JMenu kw = new JMenu("kw");
 
-    JButton buttonLinks = new JButton("<<");
+    /**
+     * 
+     */
+    private JButton buttonLinks = new JButton("<<");
 
-    JButton buttonRechts = new JButton(">>");
+    /**
+     * 
+     */
+    private JButton buttonRechts = new JButton(">>");
 
+    /**
+     * 
+     */
     private JLabel txtWoche = new JLabel("Woche");
 
+    /**
+     * 
+     */
     private JTable table = new JTable();
 
-    JScrollPane scrollPane = new JScrollPane(table);
+    /**
+     * 
+     */
+    private JScrollPane scrollPane = new JScrollPane(table);
 
+    /**
+     * 
+     */
     private JPanel contentPane = new JPanel();
 
+    /**
+     * 
+     */
     private JMenu benutzer = new JMenu("Benutzer");
 
+    /**
+     * 
+     */
     private JMenuItem benutzerAnzeigen = new JMenuItem("Benutzer Info");
 
+    /**
+     * 
+     */
     private JMenuItem benutzerAusloggen = new JMenuItem("Benutzer ausloggen");
 
+    /**
+     * 
+     */
     private JMenu termin = new JMenu("Termine");
 
+    /**
+     * 
+     */
     private JMenuItem terminBearbeiten = new JMenuItem("Terminbearbeiten");
 
+    /**
+     * 
+     */
     private JMenuItem neuerTermin = new JMenuItem("Neuer Termin");
 
+    /**
+     * 
+     */
     private JMenuItem termineExportieren = new JMenuItem("Termine exportieren");
 
+    /**
+     * 
+     */
     private JMenuItem termineImportieren = new JMenuItem("Termine importieren");
 
-    DefaultTableModel tabellenmodellGruppeA;
+    /**
+     * 
+     */
+    private DefaultTableModel tabellenmodellGruppeA;
 
-    Container mainContainer;
+    /**
+     * 
+     */
+    private Container mainContainer;
 
+    /**
+     * 
+     */
     // Spalten anlegen
-    String spalten[] = { "Uhrzeit", "Montag", "Dienstag", "Mittwoch",
-        "Donnerstag", "Freitag", "Samstag", "Sonntag" };
+    private String[] spalten =
+        {"Uhrzeit", "Mo.", "Di.", "Mi.", "Do.", "Fr.", "Sa.", "So." };
 
+    /**
+     * 
+     */
     // zeilen anlegen
-    String zeilen[][] = { { "01:00" }, { "02:00" }, { "03:00" }, { "04:00" },
-        { "05:00" }, { "06:00" }, { "07:00" }, { "08.00" }, { "09:00" },
-        { "10:00" }, { "11:00" }, { "12:00" }, { "13:00" }, { "14:00" },
-        { "15:00" }, { "16:00" }, { "17:00" }, { "18:00" }, { "19:00" },
-        { "20:00" }, { "21:00" }, { "22:00" }, { "23:00" }, { "00:00" } };
+    private String[][] zeilen =
+        {{"01:00" }, {"02:00" }, {"03:00" }, {"04:00" }, {"05:00" },
+            {"06:00" }, {"07:00" }, {"08.00" }, {"09:00" }, {"10:00" },
+            {"11:00" }, {"12:00" }, {"13:00" }, {"14:00" }, {"15:00" },
+            {"16:00" }, {"17:00" }, {"18:00" }, {"19:00" }, {"20:00" },
+            {"21:00" }, {"22:00" }, {"23:00" }, {"00:00" } };
 
+    /**
+     * 
+     */
     public KalenderGui() {
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        setSize(980, 700);
+        // setSize(980, 700);
         setLocationRelativeTo(null);
 
         Calendar cal = GregorianCalendar.getInstance();
@@ -134,9 +205,10 @@ public class KalenderGui extends JFrame implements ActionListener {
         tabellenmodellGruppeA = new DefaultTableModel(zeilen, spalten);
 
         table = new JTable(tabellenmodellGruppeA);
+        table.getTableHeader().setEnabled(false);
 
-        // Um die Spalten der Tabelle anzuzeigen muss die Tabelle
-        // über eine ScrollPane hinzugefügt werden
+        gedrueck();
+
         mainContainer.add(new JScrollPane(table));
         this.pack();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -144,10 +216,76 @@ public class KalenderGui extends JFrame implements ActionListener {
 
     }
 
-    public void actionPerformed(ActionEvent e) {
+    /**
+     * 
+     */
+
+    public void gedrueck() {
+
+        benutzerAnzeigen.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new UserInfoGUI();
+
+            }
+        });
+
+        benutzerAusloggen.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                DISPOSE_ON_CLOSE();
+                new LoginGUI();
+
+            }
+        });
+
+        neuerTermin.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                KalenderGui.this.setVisible(false);
+                KalenderGui.this.dispose();
+                new NeuenTerminHinzu();
+            }
+        });
+
+        terminBearbeiten.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+
+        termineExportieren.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+
+        info.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+
+    }
+
+    protected void DISPOSE_ON_CLOSE() {
         // TODO Auto-generated method stub
 
     }
 
+    /**
+     * .
+     * 
+     * @param e
+     */
+    public void actionPerformed(ActionEvent e) {
+
+    }
 
 }
